a2676b3084fbd512dd7cd8d46a554b8dc8944347
Abhijith-03 <127974430+Abhijith-03@users.noreply.github.com>
2023-04-11 22:46:06 +0530

    Update prog1.py

--- prog1.py
+++ prog1.py
@@ -3,5 +3,5 @@ n=int("Enter the length of the list: ")
 for i in range n:
     b=input("Enter the names to be added: ")
     a.append(b)
-print("The list is: ",a)
+print("The list created is: ",a)
     
